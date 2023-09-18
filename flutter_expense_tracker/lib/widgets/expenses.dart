762b4f250fe908272366802e1a2fe99b88075f23
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-09-19 03:29:55 +0700

    update

--- flutter_expense_tracker/lib/widgets/expenses.dart
+++ flutter_expense_tracker/lib/widgets/expenses.dart
@@ -28,15 +28,54 @@ class _ExpensesState extends State<Expenses> {
 
   void _openAddExpenseOverly() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
-      builder: (ctx) {
-        return const NewExpense();
-      },
+      builder: (ctx) => NewExpense(onAddExpense: _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Kegiatan telah dihapus"),
+        action: SnackBarAction(
+          label: "Kembali",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("Tidak ada data kegiatan. Mohon diisi kembali."),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
@@ -52,7 +91,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text("Chart area"),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: mainContent,
           ),
         ],
       ),
--- flutter_expense_tracker/lib/widgets/new_expense.dart
+++ flutter_expense_tracker/lib/widgets/new_expense.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({
+    super.key,
+    required this.onAddExpense,
+  });
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() => _NewExpenseState();
@@ -29,6 +34,43 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submitExpensesData() {
+    final enteredAmount = double.parse(_amountController.text);
+    final amountIsValid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsValid ||
+        _selectedCategory == null) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text("Masukan Tidak Valid"),
+          content: const Text(
+              "Harap pastikan judul, jumlah, tanggal dan kategori yang dimasukkan valid"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text("Oke"),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount,
+        date: _selectedDate!,
+        category: _selectedCategory,
+      ),
+    );
+
+    Navigator.pop(context);
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -39,7 +81,7 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
@@ -113,7 +155,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text("Batal"),
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submitExpensesData,
                 child: const Text("Simpan"),
               ),
             ],
