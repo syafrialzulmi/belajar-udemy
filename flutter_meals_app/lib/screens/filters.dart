b48790d9bd68fe5e69361523e5a44152db31838d
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-09-28 04:07:54 +0700

    update

--- flutter_meals_app/lib/screens/filters.dart
+++ flutter_meals_app/lib/screens/filters.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+enum Filter {
+  glutenFree,
+  laktoseFree,
+  vegetarianFree,
+  veganFree,
+}
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -9,6 +16,9 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _glutenFreeFilters = false;
+  var _laktoseFreeFilters = false;
+  var _vegetarianFreeFilters = false;
+  var _veganFreeFilters = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,34 +26,120 @@ class _FiltersScreenState extends State<FiltersScreen> {
         centerTitle: false,
         title: const Text('Filter Anda'),
       ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: _glutenFreeFilters,
-            onChanged: (isChecked) {
-              setState(() {
-                _glutenFreeFilters = isChecked;
-              });
-            },
-            title: Text(
-              'Bebas gula',
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilters,
+            Filter.laktoseFree: _laktoseFreeFilters,
+            Filter.vegetarianFree: _vegetarianFreeFilters,
+            Filter.veganFree: _veganFreeFilters,
+          });
+          return false;
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              value: _glutenFreeFilters,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilters = isChecked;
+                });
+              },
+              title: Text(
+                'Bebas gula',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Hanya sertakan makanan bebas gluten',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
             ),
-            subtitle: Text(
-              'Hanya sertakan makanan bebas gluten',
-              style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground,
-                  ),
+            SwitchListTile(
+              value: _laktoseFreeFilters,
+              onChanged: (isChecked) {
+                setState(() {
+                  _laktoseFreeFilters = isChecked;
+                });
+              },
+              title: Text(
+                'Bebas laksota',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Hanya sertakan makanan bebas laktosa',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
             ),
-            activeColor: Theme.of(context).colorScheme.tertiary,
-            contentPadding: const EdgeInsets.only(
-              left: 34,
-              right: 22,
+            SwitchListTile(
+              value: _vegetarianFreeFilters,
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFreeFilters = isChecked;
+                });
+              },
+              title: Text(
+                'Bebas Vegetarian',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Hanya sertakan makanan bebas vegetarian',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
             ),
-          )
-        ],
+            SwitchListTile(
+              value: _veganFreeFilters,
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFreeFilters = isChecked;
+                });
+              },
+              title: Text(
+                'Bebas Vegan',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              subtitle: Text(
+                'Hanya sertakan makanan bebas vegan',
+                style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
