57f7e3b0374895092d180d80f7fe565d8e698832
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-10-09 22:04:40 +0700

    update

--- flutter_meals_app/lib/screens/tabs.dart
+++ flutter_meals_app/lib/screens/tabs.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals_app/data/dummy_data.dart';
-import 'package:flutter_meals_app/models/meal.dart';
+// import 'package:flutter_meals_app/data/dummy_data.dart';
+// import 'package:flutter_meals_app/models/meal.dart';
 import 'package:flutter_meals_app/screens/categories.dart';
 import 'package:flutter_meals_app/screens/filters.dart';
 import 'package:flutter_meals_app/screens/meals.dart';
 import 'package:flutter_meals_app/widgets/main_drawer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:flutter_meals_app/providers/meals_provider.dart';
+// import 'package:flutter_meals_app/providers/meals_provider.dart';
 import 'package:flutter_meals_app/providers/favorites_provider.dart';
 import 'package:flutter_meals_app/providers/filters_provider.dart';
 
