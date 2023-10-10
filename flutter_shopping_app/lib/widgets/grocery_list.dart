524f24d4b1599f8f8e2e9d1b5975e23d25c2d873
Syafrial Zulmi <syafrialzulmi@gmail.com>
2023-10-10 12:43:14 +0700

    update

--- flutter_shopping_app/lib/widgets/grocery_list.dart
+++ flutter_shopping_app/lib/widgets/grocery_list.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/data/dummy_items.dart';
+import 'package:flutter_shopping_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) {
+          return const NewItem();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Belanja Kamu'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
